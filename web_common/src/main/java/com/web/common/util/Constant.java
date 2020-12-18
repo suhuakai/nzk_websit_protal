2f3a13b0f8c05574204a86907fcf03c1996842b7
meilongli <593746221@qq.com>
2020-12-18 14:24:42 +0800

    验证

---
+++ web_common/src/main/java/com/web/common/util/Constant.java
@@ -0,0 +1,36 @@
+package com.web.common.util;
+
+/**
+ * 常量类
+ *
+ * @author sunhua
+ * @date 2020/12/18
+ */
+public class Constant {
+
+    /**
+     * 用户状态 0停用  1启用
+     */
+    public enum UserInfoFstate {
+        UN_Enable(0, "停用"),
+        Enable(1, "启用");
+        private Integer code;
+        private String value;
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        UserInfoFstate(Integer code, String value) {
+            this.code = code;
+            this.value = value;
+        }
+
+    }
+
+
+}
